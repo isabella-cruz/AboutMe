a2135fe94c3330f3a8880f6740018b2a5e67f0ef
isabella-cruz <isabella.cruz@ifood.com.br>
2021-06-01 20:35:26 -0300

    Init commit

---
+++ app/src/main/java/com/isabella/aboutme/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.isabella.aboutme
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNicknameHandler(it)
+        }
+
+    }
+
+    //    Add a click handler to the Done button that displays the inputted text in the TextView
+//    and hides the EditText and button.
+    fun addNicknameHandler(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+//    Set the text of nickname_text to the value of nickname_edit:
+//    nicknameTextView.text = editText.text
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+//    Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
